8569cbc52fec1fea40337cf1aa18edd603e631e3
hritayan-rick <72493205+hritayan-rick@users.noreply.github.com>
2020-12-12 16:08:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var Engine = Matter.Engine,
+        Render = Matter.Render,
+        Runner = Matter.Runner,
+        Body = Matter.Body,
+        Composites = Matter.Composites,
+        MouseConstraint = Matter.MouseConstraint,
+        Mouse = Matter.Mouse,
+        World = Matter.World;
+
+    // create engine
+    var engine = Engine.create(),
+        world = engine.world;
+
+    // create renderer
+    var render = Render.create({
+        element: document.body,
+        engine: engine,
+        options: {
+            width: 800,
+            height: 600,
+            showVelocity: true
+		}
+	})
